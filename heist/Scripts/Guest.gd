dfa22bd83485e05cc640a66128c78d7236ccbc8d
Colonelpanic0 <111532750+Colonelpanic0@users.noreply.github.com>
2026-01-31 15:20:43 -0500

    Guest animations

--- heist/Scripts/Guest.gd
+++ heist/Scripts/Guest.gd
@@ -6,6 +6,8 @@ var time_until_turn = 0.0
 
 func _ready():
 	time_until_turn = randf_range(7.0, 12.0)
+	$Sprite.play("walk")
+	$Sprite.flip_h = true
 
 func _physics_process(delta):
 	velocity.x = direction * speed
@@ -14,11 +16,13 @@ func _physics_process(delta):
 	time_until_turn -= delta
 	if time_until_turn <= 0:
 		direction *= -1
+		$Sprite.flip_h = not $Sprite.flip_h
 		time_until_turn = randf_range(2.0, 7.0)
 	
 	# Turn around at walls
 	if is_on_wall():
 		direction *= -1
+		$Sprite.flip_h = not $Sprite.flip_h
 		time_until_turn = randf_range(3.0, 8.0)
 	
 	move_and_slide()
