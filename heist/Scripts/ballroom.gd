8bca7f1a9bfb93af178dac5d428a000718212077
Colonelpanic0 <111532750+Colonelpanic0@users.noreply.github.com>
2026-01-31 13:55:58 -0500

    Added singleton

--- heist/Scripts/ballroom.gd
+++ heist/Scripts/ballroom.gd
@@ -1,15 +1,14 @@
 extends Node2D
 
-@export var playable_scene :PackedScene
+@export var shadow_min :int = 2
+@export var shadow_max :int = 4
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("Start"):
-		_on_start_pressed()
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(playable_scene)
+	pass
