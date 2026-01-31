1b2c9f0801ab2680cf4ba5882ae1d48d10e8bb50
Colonelpanic0 <111532750+Colonelpanic0@users.noreply.github.com>
2026-01-31 16:18:39 -0500

    Gems and ascension

---
+++ heist/Scripts/gem.gd
@@ -0,0 +1,10 @@
+extends Area2D  # or whatever node type you're using
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	Singleton.alarmed = true
+	Singleton.gem_collected = true
+	
+	queue_free() 
