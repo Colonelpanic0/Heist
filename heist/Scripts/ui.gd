ee2be5fad9078249bd1512a68ec9f99667af7a5d
Colonelpanic0 <111532750+Colonelpanic0@users.noreply.github.com>
2026-01-31 22:02:42 -0500

    Added bullet count to status bar

--- heist/Scripts/ui.gd
+++ heist/Scripts/ui.gd
@@ -3,8 +3,10 @@ extends Control
 @onready var time_label :Label = $Time
 @onready var score_label :Label = $Lives
 @onready var lives_label :Label = $Score
+@onready var bullets_label :Label = $Bullets
 
 func _process(delta: float) -> void:
 	time_label.text = "Time: " + str(Singleton.timer)
 	lives_label.text = "Lives: " + str(Singleton.lives)
 	score_label.text = "Score: " + str(Singleton.score)
+	bullets_label.text = "Bullets: " + str(Singleton.bullets)
