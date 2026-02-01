b639d0968b018b58f4530f0f4cba41b141832209
Colonelpanic0 <111532750+Colonelpanic0@users.noreply.github.com>
2026-01-31 19:03:19 -0500

    Game is now completable - can ascend to the top level to collect the gem, then return to the bottom

--- heist/Scripts/ascend.gd
+++ heist/Scripts/ascend.gd
@@ -4,15 +4,16 @@ var ascended :bool = false
 var descended :bool = false
 
 func _ready():
-	
+	ascended = false
+	descended = false
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
+	print_debug("collide")
 	if Singleton.gem_collected and !descended:
-		body.position.y-=116
+		body.position.y+=116
 		descended=true
 	elif !Singleton.gem_collected and !ascended:
-		body.position.y+=116
+		print_debug("ascend")
+		body.position.y-=116
 		ascended=true
-	
-	queue_free() 
--- heist/Scripts/gem.gd
+++ heist/Scripts/gem.gd
@@ -3,7 +3,7 @@ extends Area2D
 func _ready():
 	body_entered.connect(_on_body_entered)
 
-func _on_body_entered(body):
+func _on_body_entered(body): 
 	Singleton.alarmed = true
 	Singleton.gem_collected = true
 	
